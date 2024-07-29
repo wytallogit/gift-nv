fbd99c76278b22bef18dffbb9e2ae899a973bc1e
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-07-29 13:44:25 -0300

    Html creater.

---
+++ script.js
@@ -0,0 +1,9 @@
+const nome = document.getElementById('nome');
+const br = document.getElementById('br');
+const tipoPresente = document.getElementById('tipo-presente');
+
+function gerar() {
+    alert(nome.value);
+    alert(br.value);
+    alert(tipoPresente.value);
+};
