a9e53d08b48fc4a33f8b6e01819a9007d52c5c29
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-07-31 23:57:27 -0300

    corrigido

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function gerar() {
     const nome = document.getElementById('nome');
     const br = document.getElementById('br');
     const tipoPresente = document.getElementById('tipo-presente');
-    const gift = document.getElementById('gift');
+    const canvas = document.getElementById('gift');
     let ctx = canvas.getContext('2d');
 
     ctx.fillStyle = 'green';
-    ctx.fillReact(10, 10, 100, 100);
+    ctx.fillRect(10, 10, 100, 100);
 
 }
