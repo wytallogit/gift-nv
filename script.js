ebc6dce6269adedeeb4b75cad2f780f9bf3aa763
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-08-01 00:03:20 -0300

    up

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function gerar() {
+function carregar() {
     const nome = document.getElementById('nome');
     const br = document.getElementById('br');
     const tipoPresente = document.getElementById('tipo-presente');
     const canvas = document.getElementById('gift');
     let ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 100, 100);
+    ctx.font = '14px serif';
+    ctx.fillText(nome.value, 10, 50);
 
 }
