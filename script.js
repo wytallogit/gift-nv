d4b3a2d4c7b6b147bd0a3bb6ec4cde8161f219c8
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-08-11 16:09:02 -0300

    new background

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function draw() {
     const valor = document.getElementById('valor').value;
     const codigo = document.getElementById('codigo').value;
     const canvas = document.getElementById('canvas');
+    canvas.style.display = 'block';
     const ctx = canvas.getContext('2d');
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -13,10 +14,13 @@ function draw() {
     ctx.font = '20px Arial';
     ctx.fillStyle = 'white';
 
-    ctx.fillText(nome, 20, 100);
-    ctx.fillText(`BR055500${br}`, 300, 50);
-    ctx.fillText(faixaEtaria, 20, 80);
+    ctx.fillText(nome, 20, 30);
+    ctx.fillText(`BR055500${br}`, 20, 55);
+    ctx.fillText(`${faixaEtaria} anos`, 20, 75);
     ctx.fillText(tipoPresente, 50, 100);
-    ctx.fillText(valor, 50, 100);
-    ctx.fillText(codigo, 50, 100);
+    ctx.fillText(valor, 350, 30);
+    ctx.fillText(codigo, 350, 55);
+
+    ctx.font = '12px Arial';
+    ctx.fillText(`KDM | Mesquita | Parnaíba importados | Shop Brasil | Casa Araújo & Kids | Avistão`, 20, 220);
 }
