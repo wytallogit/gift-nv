3ea8b55f8c3a528d12cb206baa7f2b6ed84993c3
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-08-11 13:52:01 -0300

    versão otimizada

--- script.js
+++ script.js
@@ -1,61 +1,22 @@
 function draw() {
     const nome = document.getElementById('nome').value;
-    const faixaEtaria = document.getElementById('faixa-etaria').value;
     const br = document.getElementById('br').value;
+    const faixaEtaria = document.getElementById('faixa-etaria').value;
     const tipoPresente = document.getElementById('tipo-presente').value;
     const valor = document.getElementById('valor').value;
     const codigo = document.getElementById('codigo').value;
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
 
-    carregar(nome, faixaEtaria, br, tipoPresente, valor, codigo)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-}
-
-
-function carregar(nome, faixaEtaria, br, tipoPresente, valor, codigo) {
-
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
-
-    // Preencher o fundo
-    ctx.beginPath();
-    ctx.fillStyle = "#F2F2F2";
-    ctx.fillRect(0, 0, 1050, 600);
-    ctx.closePath();
-
-    // Desenhar a curva preenchida
-    ctx.beginPath();
-    ctx.moveTo(0, 220);
-    ctx.quadraticCurveTo(200, 50, 500, 100);
-    ctx.lineTo(0, 250);
-    ctx.fillStyle = 'green';
-    ctx.fill();
-    ctx.closePath();
-
-    // Desenhar o texto
     ctx.font = '20px Arial';
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'white';
 
-    ctx.fillText(nome, 20, 50);
-    ctx.fillText(br, 20, 75);
-    ctx.fillText(faixaEtaria, 20, 95);
-    ctx.fillText(tipoPresente, 20, 115);
-    ctx.fillText(codigo, 200, 90);
-    ctx.fillText(valor, 200, 50);
-
-
-
+    ctx.fillText(nome, 20, 100);
+    ctx.fillText(`BR055500${br}`, 300, 50);
+    ctx.fillText(faixaEtaria, 20, 80);
+    ctx.fillText(tipoPresente, 50, 100);
+    ctx.fillText(valor, 50, 100);
+    ctx.fillText(codigo, 50, 100);
 }
-
-function downloadImage() {
-    let canvas = document.getElementById('canvas');
-    let url = canvas.toDataURL('image/jpeg');
-
-    let link = document.createElement('a');
-    link.href = url;
-    link.download = 'imagem.jpg';
-
-    document.body.appendChild(link);
-    link.click();
-
-    document.body.removeChild(link);
-};
