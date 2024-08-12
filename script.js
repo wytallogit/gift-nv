5325ab4969acd3e5dae92ac364d8ece24ca4ce4c
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-08-11 23:56:44 -0300

    Up

--- script.js
+++ script.js
@@ -6,21 +6,30 @@ function draw() {
     const valor = document.getElementById('valor').value;
     const codigo = document.getElementById('codigo').value;
     const canvas = document.getElementById('canvas');
-    canvas.style.display = 'block';
     const ctx = canvas.getContext('2d');
+    
+    if (nome.length == 0) {
+        alert('Preencha todos os campos!')
+    } else {
+        canvas.style.display = 'block';
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+        ctx.font = '30px Arial';
+        ctx.fillText(nome, 20, 40);
+        ctx.font = '20px Arial';
+        ctx.fillStyle = 'black';
+    
+        ctx.fillText(`BR055500${br}`, 20, 65);
+        ctx.fillText(`${faixaEtaria} anos`, 20, 85);
+    
+        ctx.textAlign = 'right';
+        ctx.fillText(`R$${Math.ceil(valor)},00`, 480, 40);
+        ctx.fillText(`Gl - ${codigo}`, 480, 65);
+        ctx.fillText(tipoPresente, 480, 75);
+    
+        ctx.textAlign = 'center';
+        ctx.font = '12px Arial';
+        ctx.fillText(`KDM | Mesquita | Parnaíba importados | Shop Brasil | Casa Araújo & Kids | Avistão`, 250, 220);
+    }
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.font = '20px Arial';
-    ctx.fillStyle = 'white';
-
-    ctx.fillText(nome, 20, 30);
-    ctx.fillText(`BR055500${br}`, 20, 55);
-    ctx.fillText(`${faixaEtaria} anos`, 20, 75);
-    ctx.fillText(tipoPresente, 50, 100);
-    ctx.fillText(valor, 350, 30);
-    ctx.fillText(codigo, 350, 55);
-
-    ctx.font = '12px Arial';
-    ctx.fillText(`KDM | Mesquita | Parnaíba importados | Shop Brasil | Casa Araújo & Kids | Avistão`, 20, 220);
 }
