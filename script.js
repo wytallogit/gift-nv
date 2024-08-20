ebd8113436509b2d6cbd6709017e733419dc9587
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-08-20 09:41:21 -0300

    Up

--- script.js
+++ script.js
@@ -8,28 +8,61 @@ function draw() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     
+
     if (nome.length == 0) {
+        alert('Coloque o nome!')
+    } else if (br.length != 3) {
+        alert('Coloque três números no BR')
+    } else if(faixaEtaria == 0 || tipoPresente == 0 || valor == 0 || codigo == 0) {
         alert('Preencha todos os campos!')
     } else {
         canvas.style.display = 'block';
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
+            
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+        
+        ctx.beginPath();
+        ctx.moveTo(0, 100);
+        ctx.quadraticCurveTo(0, 100, 0, 0);
+        ctx.lineTo(0, 250);
+        ctx.lineTo(500, 250);
+        ctx.closePath();
+        ctx.fillStyle = '#00A14B';
+        ctx.fill();
+        
+        ctx.fillStyle = 'black';
         ctx.font = '30px Arial';
         ctx.fillText(nome, 20, 40);
         ctx.font = '20px Arial';
-        ctx.fillStyle = 'black';
-    
+        
         ctx.fillText(`BR055500${br}`, 20, 65);
         ctx.fillText(`${faixaEtaria} anos`, 20, 85);
-    
+        
         ctx.textAlign = 'right';
-        ctx.fillText(`R$${Math.ceil(valor)},00`, 480, 40);
-        ctx.fillText(`Gl - ${codigo}`, 480, 65);
-        ctx.fillText(tipoPresente, 480, 75);
-    
+        ctx.fillText(`R$${Math.ceil(valor)},00`, 480, 35);
+        ctx.fillText(`Gl - ${codigo}`, 480, 60);
+        ctx.fillText(tipoPresente, 480, 80);
         ctx.textAlign = 'center';
         ctx.font = '12px Arial';
         ctx.fillText(`KDM | Mesquita | Parnaíba importados | Shop Brasil | Casa Araújo & Kids | Avistão`, 250, 220);
-    }
 
+        const img = new Image();
+        img.src = 'imagens/logo.png';
+        img.onload = function() {
+            ctx.drawImage(img, 175, 80, 150, 110);
+        }
+    };
+
+}
+
+function downloadImage() {
+    const canvas = document.getElementById('canvas');
+    const image = canvas.toDataURL('image/png'); // Corrigido para 'image/png'
+
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'gift.png';
+
+    link.click();
 }
